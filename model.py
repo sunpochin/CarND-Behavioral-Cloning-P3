8a8d637e065d6d9a4fa9024cf531d9d459a4b2f7
sunpochin <sunpochin@gmail.com>
2017-08-17 01:06:40 +0800

    Add read_image, center .

--- model.py
+++ model.py
@@ -21,32 +21,38 @@ with open(csvfilename) as csvfile:
 from sklearn.model_selection import train_test_split
 train_samples, validation_samples = train_test_split(samples, test_size=0.2)
 
-'''
+
 # create adjusted steering measurements for the side camera images
 correction = 0.3 # this is a parameter to tune
-steering_left = center_angle + correction
-steering_right = center_angle - correction
+#steering_left = center_angle + correction
+#steering_right = center_angle - correction
+leftenum = 1
+centerenum = 2
+rightenum = 3
 def read_image(name, angle, dir):
     image = cv2.imread(name)
-    center_angle = float(batch_sample[3])
-    if 1 == dir: # left
+#    center_angle = float(batch_sample[3])
+    center_angle = angle
+    if leftenum == dir: # left
         angle = center_angle + correction
-    elif 2 == dir: # center
+    elif centerenum == dir: # center
         angle = center_angle
         # drop all 0 angle!
         if 0 == angle:
-            return 4
-    elif 3 == dir: # right
+            return None, 4
+    elif rightenum == dir: # right
         angle = center_angle - correction
     else:
         pass
 
-    if image == None:
+    if image.any() == None:
         print("Invalid image path:", center_name)
-        return 4
+        return None, 4
     else:
-        return 5
-'''
+        pass
+
+    return image, angle
+
 
 
 bsize = 32
@@ -63,16 +69,27 @@ def generator(samples, batch_size=32):
                 # name on  ubuntu
                 if batch_sample[3] == 'steering':
                     continue
-                center_name = './' + traintag + batch_sample[0].strip().split('\\')[-1]
-#                name = './train-4/IMG/' + batch_sample[0].split('\\')[-1]
-#                print(name)
-                center_image = cv2.imread(center_name)
-                center_angle = float(batch_sample[3])
-
+                center_angle = float(batch_sample[3] )
                 # create adjusted steering measurements for the side camera images
                 correction = 0.3 # this is a parameter to tune
                 steering_left = center_angle + correction
                 steering_right = center_angle - correction
+
+
+                center_name = './' + traintag + batch_sample[0].strip().split('\\')[-1]
+#                name = './train-4/IMG/' + batch_sample[0].split('\\')[-1]
+#                print(name)
+                image, angle = read_image(center_name, center_angle, centerenum)
+                if (angle == 4):
+#                    print("None center image!")
+                    continue
+                images.append(image)
+                angles.append(angle)
+
+                '''
+                center_image = cv2.imread(center_name)
+                center_angle = float(batch_sample[3])
+
                 
                 angle = float(center_angle)
                 if 0.0 == angle:
@@ -90,6 +107,7 @@ def generator(samples, batch_size=32):
                     else:
                         #print("skipped angle 0!")
                         continue
+                '''
                         
                 left_name = './' + traintag + batch_sample[1].strip().split('\\')[-1]
                 left_image = cv2.imread(left_name)
@@ -134,21 +152,23 @@ validation_generator = generator(validation_samples, batch_size = 32)
 ch, row, col = 3, 160, 320  # Trimmed image format
 
 model = Sequential()
+
+# resize: https://discussions.udacity.com/t/keras-lambda-to-resize-seems-causing-the-problem/316247/3?u=sunpochin
 # cropping
 model.add(Cropping2D(cropping=((50, 30), (0, 0)), input_shape=(160,320,3)))
+# The example above crops: 50 rows pixels from the top of the image 
+#30 rows pixels from the bottom of the image
+#0 columns of pixels from the left of the image 0 columns of pixels from the right of the image
 def resize_img(input):
+    # ktf must be declared here to 'be stored in the model' and 'let drive.py use it'
+    new_width = 32
+    new_height = 32
     from keras.backend import tf as ktf
-    return ktf.image.resize_images(input, (32, 32))
+    return ktf.image.resize_images(input, (new_width, new_height))
 
-#... in the model ...
+# resize: https://discussions.udacity.com/t/keras-lambda-to-resize-seems-causing-the-problem/316247/3?u=sunpochin
 model.add(Lambda(resize_img))
-
-#model.add(Cropping2D(cropping=((80, 20), (130,130)), input_shape=(160,320,3)))
-'''
-model.add(Cropping2D(cropping=((50,20), (0,0)), input_shape=(160,320,3)))
-# The example above crops: 50 rows pixels from the top of the image 20 rows pixels from the bottom of the image
-0 columns of pixels from the left of the image 0 columns of pixels from the right of the image
-'''
+model.add(Lambda(lambda x:x / 255.0 - 0.5, input_shape = (row, col, ch) ) )
 
 # Preprocess incoming data, centered around zero with small standard deviation 
 '''
@@ -157,10 +177,7 @@ model.add(Lambda(lambda x: x/127.5 - 1.,
         output_shape=(ch, row, col)))
 #model.add(... finish defining the rest of your model architecture here ...)
 '''
-model.add(Lambda(lambda x:x / 255.0 - 0.5, input_shape = (row, col, ch) ) )
-new_width = 32
-new_height = 32
-from keras.backend import tf as ktf
+
 '''
 will have bug in drive.py
 def preprocess(image):
@@ -183,9 +200,12 @@ model.add( Conv2D(1, (5, 5),
 model.add( Conv2D(1, (5, 5), 
     padding = 'same',
     activation="relu") )
+model.add( Conv2D(1, (5, 5), 
+    padding = 'same',
+    activation="relu") )
 model.add(Flatten() )
 #model.add(Dense(1164) )
-model.add(Dense(10) )
+model.add(Dense(100) )
 model.add(Dense(1) )
 model.compile(loss='mse', optimizer='adam')
 
@@ -197,8 +217,8 @@ for i in range(32):
 #print('train_generator', train_generator)
 
 print('len(train_samples): ', len(train_samples) )
-sample_rate = 64
-epoch = 2
+sample_rate = 32
+epoch = 1
 model.fit_generator(train_generator,
                     steps_per_epoch = len(train_samples) / sample_rate, 
                     validation_data = validation_generator,
